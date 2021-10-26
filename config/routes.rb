f8beb340cad9e08e471756c7b97b3d8411b1bd90
RAC22 <robrac83@gmail.com>
2021-10-25 23:56:19 -0700

    Clean up redirects playing with controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :employees
   get 'home/index'
   #root 'home#index'
-  root 'employees#index'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
