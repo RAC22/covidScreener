21f1a83cbbf5787577c756846b5999ed99137082
RAC22 <robrac83@gmail.com>
2021-10-26 19:41:39 -0700

    Migrate questions, bootstrapify new views

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          has_many :employees, dependent: :destroy
+         has_many :questions, dependent: :destroy
 
          after_create do
            self.employees.create(first_name: "Guest", last_name: "", title: "")
+           self.questions.create(attestation: "Have you experienced any of the following within the past 14 days (not due to some other known condition or treatment)?
+           New fever or sense of having a fever (100.0°F or higher)")
+           self.questions.create(attestation: "New cough, shortness of breath, difficulty breathing, fatigue, headache, chills, sore throat, congestion, runny nose, nausea, vomiting or diarrhea")
+           self.questions.create(attestation: "New muscle aches (myalgia) not caused by a specific activity (such as physical exercise)")
+           self.questions.create(attestation: "New loss of taste or smell")
+           self.questions.create(attestation: "In the past 14 days, have you taken a COVID-19 test that used saliva, nose swab, or throat swab (not a blood test) and 1) not yet recieved results or 2) received a positive result?")
+           self.questions.create(attestation: "Within the past 14 days, has a public health or medical professional told you to self-monitor, self isolate, or self-quarantine because of concerns about COVID-19 infection?")
+           self.questions.create(attestation: "To the best of your knowledge, in the past 14 days, have you been in close contact (within 6 feet for a cumulative total of at least 15 minutes) with anyone while they had COVID-19?")
+           self.questions.create(attestation: "Would anyone living in your household answer YES to any of the previous questions?")
+           self.questions.create(attestation: "In the past 14 days, have you travelled outside the country?")
          end
+         
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_24_035956) do
+ActiveRecord::Schema.define(version: 2021_10_27_004939) do
 
   create_table "employees", force: :cascade do |t|
     t.string "first_name"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2021_10_24_035956) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_employees_on_user_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "attestation"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
