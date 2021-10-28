46f15865c1b61b16bd0f5be52f2b11954e4b324b
RAC22 <robrac83@gmail.com>
2021-10-27 20:29:00 -0700

    Format management page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :get_username
+
     def after_sign_in_path_for(resource)
-        employees_path
+        home_landing_path
+    end
+    def get_username
+      if current_user != nil
+      email = current_user.email
+      username, domain = email.split('@')
+      @username = username
       end
+    end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :employees
   get 'home/index'
+  get 'home/landing'
+  get 'home/launch'
   #root 'home#index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
