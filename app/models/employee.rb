2f814475d6df8653a45c3a028e601b78664f9826
RAC22 <robrac83@gmail.com>
2021-10-29 19:21:55 -0700

    clean up

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,14 +1,4 @@
 class Employee < ApplicationRecord
     belongs_to :user
-
-    def self.search(search)
-        if search
-            words = search.split(' ')
-            Employee.where(
-            @searchResults =Array.new(words.length, 'title LIKE ?').join(' AND '),
-            *words.map { |word| "%#{word}%" }) 
-        else
-            @searchResults = "no search =["           
-        end
-      end
+    
 end
